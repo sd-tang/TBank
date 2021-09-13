407d7a63f3fdd97cdd02145fa158a3dacba9f8bb
Christopher Tang <christopher.thg@gmail.com>
2021-09-13 21:42:04 +0930

    Add project files.

---
+++ BankAccount.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TBank
+{
+    public class BankAccount
+    {
+        //Define BankAccount attributes
+        private static int accountNumberSeed = 1234509876;
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { get; }
+
+        //Define constructor to initialize this object
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            this.Balance = initialBalance;
+            accountNumberSeed++;
+            this.Number = accountNumberSeed.ToString();
+        }
+
+        //Define actions/methods for BankAccount object
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Iris Kua", 35000);
+            Console.WriteLine($"Congratulations! A new TBank account has been created for you {account.Owner} with the amount ${account.Balance}\n" +
+                $"Your TBank account number is {account.Number}");
+        }
+    }
+}
