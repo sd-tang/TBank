e1248b108bf736e5352f1a68b9a13a467e277bb9
Christopher Tang <christopher.thg@gmail.com>
2021-09-14 21:33:43 +0930

    enclose main in try & catch

--- Program.cs
+++ Program.cs
@@ -6,15 +6,46 @@ namespace TBank
     {
         static void Main(string[] args)
         {
-            var account = new BankAccount("Iris Kua", 35000);
-            Console.WriteLine($"Congratulations! A new TBank account has been created for you {account.Owner} with the amount ${account.Balance}\n" +
+            try
+            {
+                var account = new BankAccount("Iris Kua", 35000);
+                Console.WriteLine($"Congratulations! A new TBank account has been created for you {account.Owner} with the amount ${account.Balance}\n" +
                 $"Your TBank account number is {account.Number}");
 
+                //Make some transactions and display balance
+                account.MakeWithdrawal(899, DateTime.Now, "Hisense Refrigerator");
+                Console.WriteLine($"\n Your current balance: {account.Balance}");
+                account.MakeDeposit(500, DateTime.Now, "Miri rental");
+                Console.WriteLine($"\n Your current balance: {account.Balance}");
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("\nException caught creating account with negative value");
+                Console.WriteLine($"\n{e.ToString()}");
+                return;
+            }
+            //var account = new BankAccount("Iris Kua", -2000);
+            //Console.WriteLine($"Congratulations! A new TBank account has been created for you {account.Owner} with the amount ${account.Balance}\n" +
+            //    $"Your TBank account number is {account.Number}");
+
             //Make a transaction
-            account.MakeWithdrawal(899, DateTime.Now, "Hisense Refrigerator");
-            Console.WriteLine($"\n Your current balance: {account.Balance}");
-            account.MakeDeposit(500, DateTime.Now, "Miri rental");
-            Console.WriteLine($"\n Your current balance: {account.Balance}");
+            //account.MakeWithdrawal(899, DateTime.Now, "Hisense Refrigerator");
+            //Console.WriteLine($"\n Your current balance: {account.Balance}");
+            //account.MakeDeposit(500, DateTime.Now, "Miri rental");
+            //Console.WriteLine($"\n Your current balance: {account.Balance}");
+
+            //Test initial balance to be positive
+            //BankAccount invalidAccount;
+            //try
+            //{
+            //    invalidAccount = new BankAccount("invalid", -300);
+            //}
+            //catch (ArgumentOutOfRangeException e)
+            //{
+            //    Console.WriteLine("\nException caught creating account with negative value");
+            //    Console.WriteLine($"\n{e.ToString()}");
+            //    return;
+            //}
         }
     }
 }
