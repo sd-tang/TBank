0d74df629b0df9d1e9f4e7fb8276c0f199412fc6
Christopher Tang <christopher.thg@gmail.com>
2021-09-16 05:30:06 +0930

    try and catch: 1. Negative initial balance 2. Over-withdrawal

--- Program.cs
+++ Program.cs
@@ -6,46 +6,39 @@ namespace TBank
     {
         static void Main(string[] args)
         {
-            try
-            {
-                var account = new BankAccount("Iris Kua", 35000);
-                Console.WriteLine($"Congratulations! A new TBank account has been created for you {account.Owner} with the amount ${account.Balance}\n" +
+            var account = new BankAccount("Iris Kua", 2000);
+            Console.WriteLine($"Congratulations! A new TBank account has been created for you {account.Owner} with the amount ${account.Balance}\n" +
                 $"Your TBank account number is {account.Number}");
 
-                //Make some transactions and display balance
-                account.MakeWithdrawal(899, DateTime.Now, "Hisense Refrigerator");
-                Console.WriteLine($"\n Your current balance: {account.Balance}");
-                account.MakeDeposit(500, DateTime.Now, "Miri rental");
-                Console.WriteLine($"\n Your current balance: {account.Balance}");
+            //Make a transaction
+            account.MakeWithdrawal(899, DateTime.Now, "Hisense Refrigerator");
+            Console.WriteLine($"\n Your current balance: {account.Balance}");
+            account.MakeDeposit(500, DateTime.Now, "Unit rental");
+            Console.WriteLine($"\n Your current balance: {account.Balance}");
+
+            //Test initial balance must be positive
+            BankAccount invalidAccount;
+            try
+            {
+                invalidAccount = new BankAccount("invalid", -300);
             }
             catch (ArgumentOutOfRangeException e)
             {
-                Console.WriteLine("\nException caught creating account with negative value");
+                Console.WriteLine("\n\nException caught creating account with negative value");
                 Console.WriteLine($"\n{e.ToString()}");
-                return;
+                //return;
             }
-            //var account = new BankAccount("Iris Kua", -2000);
-            //Console.WriteLine($"Congratulations! A new TBank account has been created for you {account.Owner} with the amount ${account.Balance}\n" +
-            //    $"Your TBank account number is {account.Number}");
 
-            //Make a transaction
-            //account.MakeWithdrawal(899, DateTime.Now, "Hisense Refrigerator");
-            //Console.WriteLine($"\n Your current balance: {account.Balance}");
-            //account.MakeDeposit(500, DateTime.Now, "Miri rental");
-            //Console.WriteLine($"\n Your current balance: {account.Balance}");
-
-            //Test initial balance to be positive
-            //BankAccount invalidAccount;
-            //try
-            //{
-            //    invalidAccount = new BankAccount("invalid", -300);
-            //}
-            //catch (ArgumentOutOfRangeException e)
-            //{
-            //    Console.WriteLine("\nException caught creating account with negative value");
-            //    Console.WriteLine($"\n{e.ToString()}");
-            //    return;
-            //}
+            //Test for a negative balance
+            try
+            {
+                account.MakeWithdrawal(1800, DateTime.Now, "Attempt to over-withdraw");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("\n\nException caught trying to over-withdraw");
+                Console.WriteLine($"\n{e.ToString()}");
+            }
         }
     }
 }
