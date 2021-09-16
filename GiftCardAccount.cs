edaf024ba527826f68a0794c9ff3a433ef9037cc
Christopher Tang <christopher.thg@gmail.com>
2021-09-16 20:23:08 +0930

    giftcardaccount and lambda operator

---
+++ GiftCardAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TBank
+{
+    public class GiftCardAccount : BankAccount
+    {
+        //Define attributes
+        private decimal _monthlyDeposit = 0m; //name private attributes with _ to prevent calling by mistake, and as a collection of privates
+
+        //Constructor
+
+        public GiftCardAccount(string name, decimal initialBalance, decimal monthlyDeposit = 0) : base(name, initialBalance)
+            => _monthlyDeposit = monthlyDeposit;
+    }
+}
